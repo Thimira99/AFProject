04ae8763ed8f6fb8198177c16b0a79a17b0c8810
Home <ishipigera@gmail.com>
2022-05-26 15:56:42 +0530

    Create MarkingScheme

--- Admin/admin_backend/routes/adminRoutes.js
+++ Admin/admin_backend/routes/adminRoutes.js
@@ -2,6 +2,7 @@ const express= require('express')
 const router = express.Router()
 const AdminLogin = require('../controller/adminController')
 const { postSubmissions, getSubmission, getASpecificSubmission, updateSubmission, deleteSubmission } = require('../controller/submissionsController')
+const { postMarkings, getMarking, getAMarking, updateMarking, deleteMarking } = require('../controller/markingsController')
 
 /*Login */
 router.post("/login", AdminLogin)
@@ -12,5 +13,12 @@ router.get("/submission/get",getSubmission)
 router.get("/submission/get/:id",getASpecificSubmission)
 router.put("/submission/update/:id",updateSubmission)
 router.delete("/submission/delete/:id",deleteSubmission)
+
+/*Marking Scheme routes */
+router.post("/marking/create",postMarkings)
+router.get("/marking/get",getMarking)
+router.get("/marking/get/:id",getAMarking)
+router.put("/marking/update/:id",updateMarking)
+router.delete("/marking/delete/:id",deleteMarking)
 
 module.exports=router
