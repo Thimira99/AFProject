2e34aa8c106a3e3925a3aab54bf7f9ada672c5b8
suki119 <it20045326@my.sliit.lk>
2022-06-02 11:57:02 +0530

    Added-Student-staffprofile

---
+++ Student/frontend/src/components/Staff/studentGroups/studentsGroups.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class studentsGroups extends Component {
+    render() {
+        return (
+            <div>
+                students 
+            </div>
+        );
+    }
+}
+
+export default studentsGroups;
